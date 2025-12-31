3444832a2d7cef6889a43529d81316dea2b4a695
DominicDev <s0l0estudi4r@gmail.com>
2025-12-31 23:04:35 +0100

    v0.0.2

--- src/app/1/page.tsx
+++ src/app/1/page.tsx
@@ -1,9 +1,11 @@
 import { Background } from './components/background';
+import { Menu } from './components/menu';
 
 export default function Page() {
   return (
     <>
       <Background />
+      <Menu />
     </>
   );
 }
