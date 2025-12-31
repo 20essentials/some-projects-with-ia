3c091f2b04f05577f0d6aa88a4bf83ecd1498970
DominicDev <s0l0estudi4r@gmail.com>
2025-12-31 23:11:25 +0100

    v0.0.3

--- src/app/1/components/menu.tsx
+++ src/app/1/components/menu.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
@@ -6,36 +7,35 @@ import {
   DialogDescription,
   DialogFooter,
   DialogHeader,
-  DialogTitle,
-  DialogTrigger
+  DialogTitle
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
 export function Menu() {
   return (
-    <Dialog>
+    <Dialog open>
       <form>
-        <DialogTrigger asChild>
-          <Button variant='outline'>Open Dialog</Button>
-        </DialogTrigger>
-        <DialogContent className='sm:max-w-[425px]'>
+        <DialogContent className='max-w-106.25'>
           <DialogHeader>
             <DialogTitle>Edit profile</DialogTitle>
             <DialogDescription>
               Make changes to your profile here. Click save when you&apos;re done.
             </DialogDescription>
           </DialogHeader>
+
           <div className='grid gap-4'>
             <div className='grid gap-3'>
               <Label htmlFor='name-1'>Name</Label>
-              <Input id='name-1' name='name' defaultValue='Pedro Duarte' />
+              <Input id='name-1' defaultValue='Pedro Duarte' />
             </div>
+
             <div className='grid gap-3'>
               <Label htmlFor='username-1'>Username</Label>
-              <Input id='username-1' name='username' defaultValue='@peduarte' />
+              <Input id='username-1' defaultValue='@peduarte' />
             </div>
           </div>
+
           <DialogFooter>
             <DialogClose asChild>
               <Button variant='outline'>Cancel</Button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { redirect } from 'next/navigation';
 import Link from 'next/link';
 
 const arrayOfProjecst = [{ label: 'GroqService + CerebraService' }];
 
 export default function Page() {
+  redirect('/1');
   return (
     <>
       {arrayOfProjecst.map(({ label }, i) => (
